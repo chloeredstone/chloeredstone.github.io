eb9ca23dfea22f1ad99610bc7a9e488084ee36e1
chloeredstone <107275548+chloeredstone@users.noreply.github.com>
2022-07-08 09:00:33 -0700

    Add menutoggle

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hi! Welcome to my design portfolio!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+   x.className += ' responsive'; {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
